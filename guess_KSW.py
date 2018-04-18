afbe0c53bf6cf74c9ca4c79896a24ab9d4e15037
ILS-Z399 <30897280+ILS-Z399@users.noreply.github.com>
2018-04-18 08:26:06 -0400

    Adds peer_review.txt and some error-handling

--- guess_KSW.py
+++ guess_KSW.py
@@ -8,30 +8,34 @@ assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 guess = 0
 guesses = 0
 
+range_low = 1
+range_high = 20
+
 # Intro to the game, and welcoming the player.
 print("Welcome to Guess a Number!")
 
 
 
 # Random ranges for the user to guess between
-number = random.randrange(1, 11)
+number = random.randrange(range_low, range_high)
 
 
 # Assigning guess to be converted into int, and asking for the users input
 
 while guess != number:
-    guess = int(input("Please Guess a number: "))
-    guesses = guesses + 1
+    try: 
+        guess = int(input(f"Please Guess a number between {range_low} and {range_high}: "))
+        guesses = guesses + 1
 
-
-# Sees if the user's guesses are too low, too high, or the correct answer.
-
-    if guess < number:
-        print('Too Low! Guess again.')
-    elif guess > number:
-        print("Too High! Guess again.")
-    else:
-        print("You got it! Great job!")
+    # Sees if the user's guesses are too low, too high, or the correct answer.
+        if guess < number:
+            print('Too Low! Guess again.')
+        elif guess > number:
+            print("Too High! Guess again.")
+        else:
+            print("You got it! Great job!")
+    except:
+        print('Please guess a number.')
 
 
 
@@ -50,15 +54,17 @@ number = 20
 # Assigning guess to be converted into int, and asking for the users input
 
 while guess != number:
-    guess = int(input("Please Guess my age: "))
-    guesses = guesses + 1
+    try:
+        guess = int(input("Please Guess my age: "))
+        guesses = guesses + 1
 
+    # Sees if the user's guesses are too low, too high, or the correct answer.
 
-# Sees if the user's guesses are too low, too high, or the correct answer.
-
-    if guess < number:
-        print('Nope! Too low. Guess again.')
-    elif guess > number:
-        print("Nope! Too high Guess again.")
-    else:
-        print("You got my age! Great job!")
+        if guess < number:
+            print('Nope! Too low. Guess again.')
+        elif guess > number:
+            print("Nope! Too high Guess again.")
+        else:
+            print("You got my age! Great job!")
+    except:
+        print("Please choose a number.")
